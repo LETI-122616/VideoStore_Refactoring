84472fc58e795398c9057b6fa60c3e327d3db790
LETI_122607 <gctso@iscte-iul.pt>
2025-10-31 15:53:54 +0000

    V4->V5

--- src/ficha3/Customer.java
+++ src/ficha3/Customer.java
@@ -4,51 +4,51 @@ import java.util.Vector;
 
 public class Customer
 {
-	private String			_name;
-	private Vector<Rental>	_rentals	= new Vector<Rental>();
+    private String			_name;
+    private Vector<Rental>	_rentals	= new Vector<Rental>();
 
-	public Customer(String _name)
-	{
-		this._name = _name;
-	}
+    public Customer(String _name)
+    {
+        this._name = _name;
+    }
 
-	public void addRental(Rental arg)
-	{
-		_rentals.addElement(arg);
-	}
+    public void addRental(Rental arg)
+    {
+        _rentals.addElement(arg);
+    }
 
-	public String getName()
-	{
-		return _name;
-	}
+    public String getName()
+    {
+        return _name;
+    }
 
-	public String statement()
-	{
-		double totalAmount = 0;
-		int frequentRenterPoints = 0;
+    public String statement()
+    {
+        // header
+        String result = "Rental Record for " + getName() + "\n";
 
-		// header
-		String result = "Rental Record for " + getName() + "\n";
-		
-		for (Rental each: _rentals)
-		{
+        for (Rental each: _rentals)
+            result += "\t" + each.getMovie().getTitle() + "\t" + each.getAmount() + "\n";
 
-			// add frequent renter points
-			frequentRenterPoints++;
+        // add footer lines
+        result += "Amount owed is " + getTotalAmount() + "\n";
+        result += "You earned " + getTotalFrequentRenterPoints() + " frequent renter points";
+        return result;
+    }
 
-			// add bonus for a two day new release rental
-			if ((each.getMovie().getPriceCode() == Movie.Code.NEW_RELEASE) && each.getDaysRented() > 1)
-				frequentRenterPoints++;
+    public int getTotalFrequentRenterPoints()
+    {
+        int frequentRenterPoints = 0;
+        for (Rental each: _rentals)
+            frequentRenterPoints += each.getFrequentRentalPoints();
+        return frequentRenterPoints;
+    }
 
-			// show figures for this rental
-			result += "\t" + each.getMovie().getTitle() + "\t" +  each.getAmount() + "\n";
-			totalAmount +=  each.getAmount();
-		}
-
-		// add footer lines
-		result += "Amount owed is " + totalAmount + "\n";
-		result += "You earned " + frequentRenterPoints + " frequent renter points";
-		return result;
-	}
-
+    public double getTotalAmount()
+    {
+        double totalAmount = 0;
+        for (Rental each: _rentals)
+            totalAmount += each.getAmount();
+        return totalAmount;
+    }
 }
